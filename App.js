9c106736581d02713cbc3cf564141eb8bfcc641c
SeoKangHyeok <kanghyeok93@naver.com>
2022-04-09 15:48:53 +0900

    feat: Add webview package

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import {SafeAreaView, Text} from 'react-native';
 import React from 'react';
+import {WebView} from 'react-native-webview';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>App</Text>
+    <SafeAreaView style={{height: '100%'}}>
+      <WebView source={{uri: 'https://www.metavv.com/'}} />
     </SafeAreaView>
   );
 };
