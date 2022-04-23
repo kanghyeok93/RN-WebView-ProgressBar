7f8a8dc46acd4935107e382d9923a286fd527e91
SeoKangHyeok <kanghyeok93@naver.com>
2022-04-23 22:36:44 +0900

    feat: Add progress package
    webView loading feature implementation #1

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import {SafeAreaView} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {WebView} from 'react-native-webview';
 
 const App = () => {
+  const [progress, setProgress] = useState(0);
+  const [isLoaded, setLoaded] = useState(false);
+
   return (
     <SafeAreaView style={{height: '100%'}}>
+      <Progress.Bar
+        progress={progress}
+        width={null}
+        borderWidth={0}
+        borderRadius={0}
+        color="#27D6FF"
+        unfilledColor="#E7E7E7"
+      />
       <WebView
         source={{uri: 'https://www.metavv.com/'}}
         // loading progress 상태 체크
